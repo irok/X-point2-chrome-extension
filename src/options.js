317f462d9e1b6af340c1538e0f65a9207d6c0ac2
irok <t.irokawa@gmail.com>
2019-06-28 23:18:36 +0900

    options.jsとpopup.jsはbackground.jsからオブジェクトを取ってくるよう修正

--- src/options.js
+++ src/options.js
@@ -1,34 +1,34 @@
-import Credential from './credential';
-import login from './login';
-import storage from './storage';
-
-async function save_options() {
+function save_options() {
   const status = document.getElementById('status');
   const domCd = document.getElementById('domCd').value;
   const user = document.getElementById('user').value;
   const pass = document.getElementById('pass').value;
 
-  const crdt = (new Credential(storage)).assign({domCd, user, pass});
+  chrome.runtime.getBackgroundPage(async ({Credential, storage, login}) => {
+    const crdt = (new Credential(storage)).assign({domCd, user, pass});
 
-  try {
-    if (await login(crdt, {test: true})) {
-      await crdt.save();
-      status.textContent = '保存しました';
-    } else {
-      status.textContent = 'ログイン情報が正しくありません';
+    try {
+      if (await login(crdt, {test: true})) {
+        await crdt.save();
+        status.textContent = '保存しました';
+      } else {
+        status.textContent = 'ログイン情報が正しくありません';
+      }
+    } catch (err) {
+      status.textContent = err.message;
     }
-  } catch (err) {
-    status.textContent = err.message;
-  }
-  setTimeout(() => { status.textContent = ''; }, 3000);
+    setTimeout(() => { status.textContent = ''; }, 3000);
+  });
 }
 
-async function restore_options() {
-  const crdt = await Credential.retrieve(storage);
+function restore_options() {
+  chrome.runtime.getBackgroundPage(async ({Credential, storage}) => {
+    const crdt = await Credential.retrieve(storage);
 
-  document.getElementById('domCd').value = crdt.domCd;
-  document.getElementById('user').value = crdt.user;
-  document.getElementById('pass').value = crdt.pass;
+    document.getElementById('domCd').value = crdt.domCd;
+    document.getElementById('user').value = crdt.user;
+    document.getElementById('pass').value = crdt.pass;
+  });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,9 @@
-import Credential from './credential';
-import login from './login';
-import storage from './storage';
-
-async function open_xpoint() {
-  const crdt = await Credential.retrieve(storage);
-  console.log(await login(crdt));
-  window.open('https://xpt.gmo-media.jp/xpoint/login.jsp?login=site', '_blank');
+function open_xpoint() {
+  chrome.runtime.getBackgroundPage(async ({Credential, storage, login}) => {
+    const crdt = await Credential.retrieve(storage);
+    console.log(await login(crdt));
+    window.open('https://xpt.gmo-media.jp/xpoint/login.jsp?login=site', '_blank');
+  });
 }
 
 document.getElementById('open').addEventListener('click', open_xpoint);
