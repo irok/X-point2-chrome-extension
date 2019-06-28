8e725b5cb9f59943ba1e8c8cf69bcae7358407f3
irok <t.irokawa@gmail.com>
2019-06-28 10:25:27 +0900

    popup.jsもビルド対象にした

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/options.js',
+  entry: {
+    'options.js': './src/options.js',
+    'popup.js': './src/popup.js'
+  },
   output: {
-    filename: 'options.js',
+    filename: '[name]',
     path: path.resolve(__dirname, 'dist/js')
   }
 };
