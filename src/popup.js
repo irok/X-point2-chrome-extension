2fbf9b1631ca45b8b4d13907904cd69a960c573f
irok <t.irokawa@gmail.com>
2019-07-26 15:09:06 +0900

    既存機能をReactに組み込んだ

--- src/popup.js
+++ src/popup.js
@@ -1,44 +1,62 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+const FormLinks = (props) => (
+  props.bookmarks.map((bkm, index) => (
+    <p key={index}>
+      <a href={bkm.url} onClick={(event) => props.onClick(event, bkm)}>
+        {bkm.title}
+      </a>
+    </p>
+  ))
+);
+
 class PopupApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: []
+    };
+    chrome.runtime.getBackgroundPage(this.loadBookmarks.bind(this));
+  }
+
+  async loadBookmarks({credential, service}) {
+    try {
+      const crdt = await credential.load();
+      await service.login(crdt);
+
+      this.setState({
+        bookmarks: await service.getBookmarkList()
+      });
+    } catch(e) {}
+  }
+
   render() {
     return (
       <div>
-        <button id="open">X-pointを開く</button>
-        <div id="forms"></div>
+        <button onClick={this.handleClickOpenSite}>
+          X-pointを開く
+        </button>
+        <FormLinks
+          bookmarks={this.state.bookmarks}
+          onClick={this.handleClickFormLink}
+        ></FormLinks>
       </div>
     );
+  }
+
+  handleClickOpenSite() {
+    chrome.runtime.getBackgroundPage(({service}) => {
+      service.openSite();
+    });
+  }
+
+  handleClickFormLink(event, bkm) {
+    event.preventDefault();
+    chrome.runtime.getBackgroundPage(({service}) => {
+      service.openForm(bkm);
+    });
   }
 }
 
 ReactDOM.render(<PopupApp />, document.getElementById('popup-app'));
-
-chrome.runtime.getBackgroundPage(async ({credential, service}) => {
-  try {
-    const crdt = await credential.load();
-    await service.login(crdt);
-
-    const bookmarks = await service.getBookmarkList();
-    for (let bkm of bookmarks) {
-      const a = document.createElement('a');
-      a.setAttribute('href', service.constructor.url(bkm.pathname));
-      a.addEventListener('click', (event) => {
-        event.preventDefault();
-        service.openForm(bkm);
-      });
-      a.textContent = bkm.title;
-      const div = document.createElement('div');
-      div.appendChild(a);
-      document.getElementById('forms').appendChild(div);
-    }
-  } catch(e) {}
-});
-
-function openSite() {
-  chrome.runtime.getBackgroundPage(({service}) => {
-    service.openSite();
-  });
-}
-
-document.getElementById('open').addEventListener('click', openSite);
