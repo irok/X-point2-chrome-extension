8dd652e33b3babcd7f834ff81087784337e70904
irok <t.irokawa@gmail.com>
2019-12-02 12:05:10 +0900

    承認待ちの数をバッジに表示

--- src/background.js
+++ src/background.js
@@ -15,3 +15,22 @@ Object.assign(window, {
   },
   service: new Service(http)
 });
+
+// 承認待ちの数をバッジに表示する
+async function showPendingApprovalCount() {
+  const wkflCnt = await service.getWkflCnt();
+  const wkflList = Array.prototype.slice.call(wkflCnt.querySelectorAll('wkfl'));
+  const wkfl = wkflList.filter(_ => _.querySelector('type').textContent === '0')[0];
+  if (wkfl) {
+    chrome.browserAction.setBadgeText({
+      text: wkfl.querySelector('count').textContent
+    });
+  }
+}
+
+window.addEventListener('load', async () => {
+  try {
+    await service.login(await credential.load());
+    showPendingApprovalCount();
+  } catch(e) {}
+});
