3606dc1d3a889e38d7f4b976ffb9a2b494a8aea4
irok <t.irokawa@gmail.com>
2019-12-06 22:44:43 +0900

    add comments

--- src/wkfllist.js
+++ src/wkfllist.js
@@ -18,12 +18,15 @@ export default class WkflList extends Cache {
 
   // return Promise
   set(wkfllist) {
+    // 要素がなければキャッシュをクリアする（空配列で上書き）
     if (!wkfllist) {
       wkfllist = [];
     }
+    // 要素が1つのときは単一オブジェクトになるので配列にする
     else if (!Array.isArray(wkfllist)) {
       wkfllist = [wkfllist];
     }
+
     return this._set({wkfllist});
   }
 }
