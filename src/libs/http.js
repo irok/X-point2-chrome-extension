03b716654f3471eb3a1ee64a9aece4e02280d929
irok <t.irokawa@gmail.com>
2019-12-27 15:44:02 +0900

    通信のタイムアウトをコントロール

--- src/libs/http.js
+++ src/libs/http.js
@@ -1,13 +1,23 @@
 async function request(url, options = {}) {
+  // タイムアウトをコントロールする
+  const controller = new AbortController();
+  options.signal = controller.signal;
+  setTimeout(() => { controller.abort(); }, 2000);
+
+  // 通信エラーは例外を投げる (awaitによりrejectがthrowされる)
   const resp = await fetch(url, options);
   if (!resp.ok) {
+    // 200 OK 以外は例外を投げる
     throw new Error(`${resp.status} - ${resp.statusText}`);
   }
+
+  // 生データとcharsetを取り出し
   const blob = await resp.blob();
   const charset = resp.headers.get('Content-Type').split(/;\s*/)
     .filter(part => /^charset=/.test(part))[0]
     .split('=')[1];
 
+  // FileReaderで文字コード変換して返す
   return new Promise((resolve) => {
     const reader = new FileReader();
     reader.onload = () => resolve(reader.result);
