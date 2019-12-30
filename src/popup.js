abb38e7ff0ef361a7cf7fa10b6a94b02fb3062b4
irok <t.irokawa@gmail.com>
2019-12-30 19:30:22 +0900

    PopupApps.jsxもFunctional Componentにして依存性をpopup.jsに移した

--- src/popup.js
+++ src/popup.js
@@ -3,4 +3,34 @@ import ReactDOM from 'react-dom';
 import PopupApp from './components/PopupApp.jsx';
 import './popup.scss';
 
-ReactDOM.render(<PopupApp />, document.getElementById('popup-app'));
+chrome.runtime.getBackgroundPage(async ({cache, credential, login, service}) => {
+  const props = {
+    openSite() {
+      service.openSite();
+    },
+    openForm(event, form) {
+      event.preventDefault();
+      service.openForm(form);
+    }
+  };
+
+  if ((await credential.load()).user === '') {
+    props.error = 'アイコンを右クリックし、オプションからログイン情報を設定してください。';
+  } else {
+    try {
+      if (await login()) {
+        const [bookmarks, wkfllist] = await Promise.all([
+          cache.bookmarks(),
+          cache.wkfllist()
+        ]);
+        Object.assign(props, {bookmarks, wkfllist});
+      } else {
+        props.error = 'ログインできませんでした。';
+      }
+    } catch (e) {
+      props.error = 'ネットワークエラーが発生しました。社外の場合はVPNを確認してください。';
+    }
+  }
+
+  ReactDOM.render(<PopupApp {...props}/>, document.getElementById('popup-app'));
+});
