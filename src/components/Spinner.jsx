e839487946b58572292dc085ffd7b60b386c7116
irok <t.irokawa@gmail.com>
2020-01-07 21:56:16 +0900

    スピナーコンポーネントとスタイルを書く

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ScaleLoader from "react-spinners/ScaleLoader";
+
+export default () => (
+  <div className="spinner">
+    <ScaleLoader color="#ffab4d"/>
+  </div>
+);
